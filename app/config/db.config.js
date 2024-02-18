2f58723c9b2e6e833e4f8811b318d5e986d1750d
Felipe Borges <69490840+FelipeB0rges@users.noreply.github.com>
2024-02-18 19:40:02 -0300

    att

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,19 +1,12 @@
 let config;
 
-if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
-    config = {
-      HOST: "mysql.devfelipeborges.com.br",
-      USER: "devfelipeborge02",
-      PASSWORD: "lgyIZ9aw",
-      DB: "devfelipeborge02"
-    };
-  } else {
-  config = {
-    HOST: "mysql.kanzparty.com.br",
-    USER: "kanzparty",
-    PASSWORD: "HFt2pR7kB5",
-    DB: "kanzparty"
-  };
-}
+
+config = {
+  HOST: "mysql.kanzparty.com.br",
+  USER: "kanzparty",
+  PASSWORD: "HFt2pR7kB5",
+  DB: "kanzparty"
+};
+
 
 module.exports = config;
