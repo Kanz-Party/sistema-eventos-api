6e2198b564e8fce09405070d06f44452d8c200ad
Victor Tramontina <108145998+VicTramontina@users.noreply.github.com>
2024-02-18 19:22:34 -0300

    att

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-  HOST: "mysql.devfelipeborges.com.br",
-  USER: "devfelipeborge02",
-  PASSWORD: "lgyIZ9aw",
-  DB: "devfelipeborge02"
-};
+let config;
+
+if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+    config = {
+      HOST: "mysql.devfelipeborges.com.br",
+      USER: "devfelipeborge02",
+      PASSWORD: "lgyIZ9aw",
+      DB: "devfelipeborge02"
+    };
+  } else {
+  config = {
+    HOST: "mysql.kanzparty.com.br",
+    USER: "kanzparty",
+    PASSWORD: "HFt2pR7kB5",
+    DB: "kanzparty"
+  };
+}
+
+module.exports = config;
